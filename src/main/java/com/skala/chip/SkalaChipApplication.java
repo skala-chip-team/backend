8540246338dc1b136ada94bea30a424a5d5286ba
kareawe <kimminjae1573@naver.com>
2026-05-11 10:14:51 +0900

    initial commit with ci cd

---
+++ src/main/java/com/skala/chip/SkalaChipApplication.java
@@ -0,0 +1,24 @@
+package com.skala.chip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class SkalaChipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkalaChipApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "skala-chip-backend is running";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
