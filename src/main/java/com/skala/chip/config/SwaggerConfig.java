b9a3bd4e929404955520801dc06d0fe1f0ee26a2
sumin <sonsony816@gmail.com>
2026-05-15 17:32:41 +0900

    Feat : Swagger UI 및 JWT 인증 설정 추가

---
+++ src/main/java/com/skala/chip/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.skala.chip.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Skala Chip API", version = "v1")
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
